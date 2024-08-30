65e21e3c035a37c4529f4b39d9a4cb28c07efaa4
mriaus <55255215+mriaus@users.noreply.github.com>
2024-08-30 14:18:12 +0200

    Initial Commit

---
+++ ShouldWeWalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShouldWeWalk
+//
+//  Created by Marcos on 30/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
